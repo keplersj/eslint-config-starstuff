651c279ea795def0d12c4fca0a21b3b9d459a0af
Kepler Sticka-Jones <kepler+git@stickajones.org>
2020-01-25 16:37:12 -0700

    feat(React): add rules for hooks

--- react.js
+++ react.js
@@ -4,5 +4,10 @@ module.exports = {
     react: {
       version: "detect" // React version. "detect" automatically picks the version you have installed.
     }
+  },
+  plugins: ["react-hooks"],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   }
 };
