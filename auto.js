1f386ee89427070ca2522e7de78d88a7a60b0659
Kepler Sticka-Jones <kepler+git@stickajones.org>
2020-01-25 16:35:44 -0700

    feat: add configuration for react-dom

--- auto.js
+++ auto.js
@@ -5,6 +5,7 @@ module.exports = {
     "starstuff",
     (isDep("typescript") || isDevDep("typescript")) && "starstuff/typescript",
     isAnyDep("react") && "starstuff/react",
-    isDevDep("jest") && "starstuff/jest"
+    isDevDep("jest") && "starstuff/jest",
+    isAnyDep("react-dom") && "starstuff/react-dom"
   ].filter(Boolean)
 };
